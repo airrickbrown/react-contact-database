e39fa9e73022d5f97032628ae1728d05548302ec
airrickbrown <airrickbrown@gmail.com>
2024-06-01 04:08:14 -0700

    add changes

--- react-contacts/src/App.jsx
+++ react-contacts/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     { name: "Benjamin Oswusu", phone: "0234534380", location: "Tarkwa" },
     { name: "Ameyaw Daniel", phone: "0567890987", location: "Accra" },
     { name: "Juliet Anim", phone: "0247890987", location: " Techiman" },
-    { name: "Robert Calvin", phone: "0277890987", location: "Kumasi" },
+    
 
   ]);
 
